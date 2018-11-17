590cba9aeb99349a7f343b2d88b9656e0abfa52b
Amir Harandi <a.eslami76@gmail.com>
2018-11-17 23:44:49 +0000

    Add fetchWeather action creator

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '6843d291ddcbf0eab87af6d457467826';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},UK`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
