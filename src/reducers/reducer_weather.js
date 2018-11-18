e0b3fbfb09946a2009d8ce11d993a6a330c2fe2b
Amir Harandi <a.eslami76@gmail.com>
2018-11-18 00:14:36 +0000

    Remove console.log()

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,6 @@
 import { FETCH_WEATHER } from '../actions';
 
 export default function(state = [], action) {
-  console.log(action);
   switch (action.type) {
     case FETCH_WEATHER:
       return [action.payload.data, ...state];
