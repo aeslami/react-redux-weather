1de7b696d4ee834e588c78c08bf50c753248cf4f
Amir Harandi <a.eslami76@gmail.com>
2018-11-18 19:16:24 +0000

    Add average line to Chart component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+} from 'react-sparklines';
+
+const average = data =>
+  Math.round(
+    data.reduce(function(a, b) {
+      return a + b;
+    }) / data.length
+  );
 
 const Chart = ({ data, color }) => {
   return (
     <div>
       <Sparklines height={120} width={300} data={data}>
         <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <p>{average(data)}</p>
     </div>
   );
 };
