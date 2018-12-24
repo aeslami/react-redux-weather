6e7f624e6bfdfea2d86fe104739bd28812ec8394
Amir Harandi <a.eslami76@gmail.com>
2018-12-24 14:45:43 +0000

    Fix bug caused by not finding the city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,9 @@ import { FETCH_WEATHER } from '../actions';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return [action.payload.data, ...state];
+      return action.payload.status === 200
+        ? [action.payload.data, ...state]
+        : state;
     default:
       return state;
   }
